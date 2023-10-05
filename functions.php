9a8f8f2e87b2756deea2610043dd6cb0e94df2e7
Almuheetu Shihab <almuheetu@gmail.com>
2023-10-06 00:52:44 +0600

    function call

--- functions.php
+++ functions.php
@@ -12,6 +12,15 @@ add_theme_support( 'title-tag');
 
 function shihab_css_js_file_calling(){
     wp_enqueue_style('shihab-style', get_stylesheet_uri());
+    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.3.2', 'all');
+    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style('custom');
+
+
+    //jQuery calling
+    // wp_enqueue_script( $handle:string, $src:string, $deps:array, $ver:string|boolean|null, $in_footer:boolean )
+    wp_enqueue_script('jquery');
 }
 
-add_action('wp_enqueue_scripts', 'shihab_css_js_file_calling');
+add_action('wp_enqueue_scripts', 'shihab_css_js_file_calling'); 
