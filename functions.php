80b679344c252939f6b36b7eadfc916768aaac7d
Almuheetu Shihab <almuheetu@gmail.com>
2023-10-05 14:36:41 +0600

    css file link

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ add_theme_support( 'title-tag');
 // Theme CSS and jQuery File calling
 
 function shihab_css_js_file_calling(){
-
+    wp_enqueue_style('shihab-style', get_stylesheet_uri());
 }
+
 add_action('wp_enqueue_scripts', 'shihab_css_js_file_calling');
