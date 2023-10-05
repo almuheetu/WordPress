b853b6fa30e0cdd7a06aa1b5ec85ffb402fbd3db
Almuheetu Shihab <almuheetu@gmail.com>
2023-10-05 13:40:29 +0600

    function add

--- functions.php
+++ functions.php
@@ -5,5 +5,12 @@
 
 
 // Theme Title
-
 add_theme_support( 'title-tag');
+
+
+// Theme CSS and jQuery File calling
+
+function shihab_css_js_file_calling(){
+
+}
+add_action('', 'shihab_css_js_file_calling')
