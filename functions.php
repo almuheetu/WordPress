8140669727f338a30182c585d78eac1e318cac47
Almuheetu Shihab <almuheetu@gmail.com>
2023-10-10 23:45:00 +0600

    added action

--- functions.php
+++ functions.php
@@ -25,12 +25,10 @@ function shihab_css_js_file_calling(){
 }
 
 add_action('wp_enqueue_scripts', 'shihab_css_js_file_calling'); 
+add_action('wp_enqueue_scripts', 'shihab_css_js_file_calling'); 
 
 
 // Theme Function 
-function shihab_customizar_register($wp_customize){
-    $wp_customize->
-}
 function shihab_customizar_register($wp_customize){
     $wp_customize->
 }
