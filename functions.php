ad545a09902b32d44357839fbd0b9bb43b6aa08d
Almuheetu Shihab <almuheetu@gmail.com>
2023-10-10 23:46:24 +0600

    added theme title

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
 
 // Theme Title
 add_theme_support( 'title-tag');
+add_theme_support( 'title-tag');
 
 
 // Theme CSS and jQuery File calling
@@ -19,10 +20,6 @@ function shihab_css_js_file_calling(){
 
 
     //jQuery calling
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.3.2', 'true');
-    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true');
-}
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.3.2', 'true');
     wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true');
