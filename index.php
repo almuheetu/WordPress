2b6d1d776f0416f7cadc86fd4002bf7a42bbb8c5
Almuheetu Shihab <almuheetu@gmail.com>
2023-10-10 23:49:37 +0600

    all fixed header

--- index.php
+++ index.php
@@ -27,16 +27,6 @@
         </div>
     </div>
 </div>
-<div id="header_area">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt=""></a>
-            </div>
-            <div class="col-md-9"></div>
-        </div>
-    </div>
-</div>
 
 
 
