66c36a012476b1b7646d0e0051e085c512206629
Almuheetu Shihab <almuheetu@gmail.com>
2023-12-01 23:55:53 +0600

    all pages footer fixed

--- archive.php
+++ archive.php
@@ -19,23 +19,4 @@ get_header(); ?>
 
 
 
-<footer id="footer_area">
-
-<section id="copyright_area">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <p><?php echo get_theme_mod('shihab_copyright_section'); ?></p>
-      </div>
-    </div>
-  </div>
-</section>
-</footer>
-
-
-
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+<?php get_footer(); ?>
