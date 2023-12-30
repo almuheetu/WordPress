3400db55c012445944e41a18e7c1ad9b0aaa7158
Almuheetu Shihab <almuheetu@gmail.com>
2023-12-30 20:05:28 +0600

    all post add

--- index.php
+++ index.php
@@ -8,7 +8,23 @@ get_header(); ?>
  <div class="container">
     <div class="row">
         <div class="col-md-9">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) : the_post();
+            ?>
+            <div class="blog_area">
+
             <?php the_content(); ?>
+            </div>
+
+                <?php 
+                    endwhile;
+                    else :
+                        _e('No post found');
+                    endif;
+                ?>
+
+           
         </div>
         <div class="col-md-3">
             <h2>this is siteber area</h2>
